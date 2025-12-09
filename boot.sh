aa1bf359e53e0993bf65fb922176702caed9c70a
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-12-09 17:58:45 -0300

    a

--- boot.sh
+++ boot.sh
@@ -3,16 +3,15 @@
 # Set install mode to online since boot.sh is used for curl installations
 export OMARCHY_ONLINE_INSTALL=true
 
-ansi_art='                 ▄▄▄                                                   
- ▄█████▄    ▄███████████▄    ▄███████   ▄███████   ▄███████   ▄█   █▄    ▄█   █▄ 
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   █▀   ███   ███  ███   ███
-███   ███  ███   ███   ███ ▄███▄▄▄███ ▄███▄▄▄██▀  ███       ▄███▄▄▄███▄ ███▄▄▄███
-███   ███  ███   ███   ███ ▀███▀▀▀███ ▀███▀▀▀▀    ███      ▀▀███▀▀▀███  ▀▀▀▀▀▀███
-███   ███  ███   ███   ███  ███   ███ ██████████  ███   █▄   ███   ███  ▄██   ███
-███   ███  ███   ███   ███  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
- ▀█████▀    ▀█   ███   █▀   ███   █▀   ███   ███  ███████▀   ███   █▀    ▀█████▀ 
-                                       ███   █▀                                  '
+ansi_art=' 
+▄███████▄   ▄████████  █▄          ▄███████▄  ▄███████▄   ▄███████▄  █████████▄ 
+███     ███ ███▀    ██  ███           ▀███▀   ███     ███ ███     ███ ███     ██
+███     █▀  ███     ▀   ███            ███    ███     ███ ███         ███        
+███▄▄▄▄▄    ███         ███            ███    ███     ███  ▀███████▄  ███▄▄▄▄▄   
+███▀▀▀▀     ███         ███            ███   ██████████▀          ███ ███▀▀▀▀ ▄   
+███         ███     ▄   ███            ███    ███         ███     ███ ███     ██   
+███     ██  ███▄    ██  ███     █▄    █████   ███          ▀███████▀  ███     ███
+█████████▀   ▀███████▀   ▀███████▀   ███████  ██              ▀▀▀      ▀███████▀ '
 
 clear
 echo -e "\n$ansi_art\n"
@@ -20,9 +19,9 @@ echo -e "\n$ansi_art\n"
 sudo pacman -Syu --noconfirm --needed git
 
 # Use custom repo if specified, otherwise default to basecamp/omarchy
-OMARCHY_REPO="${OMARCHY_REPO:-basecamp/omarchy}"
+OMARCHY_REPO="emiliano-gandini-outeda/eclipseOS"
 
-echo -e "\nCloning Omarchy from: https://github.com/${OMARCHY_REPO}.git"
+echo -e "\nCloning EclipseOS from: https://github.com/emiliano-gandini-outeda/eclipseOS.git"
 rm -rf ~/.local/share/omarchy/
 git clone "https://github.com/${OMARCHY_REPO}.git" ~/.local/share/omarchy >/dev/null
 
